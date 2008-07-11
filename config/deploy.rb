c5bd957cde9089f7d2612516817220aa747998dc
Cameron Ketcham <cketcham@gmail.com>
2008-07-10 21:53:48 -0700

    link to the music files which have been uploaded

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,5 +23,6 @@ role :db,  "jump.ath.cx", :primary => true
 #moves over server config files after deploying the code
 task :update_config, :roles => [:app] do
   run "cp -rf #{shared_path}/config/* #{release_path}/config"
+  run "ln -s #{shared_path}/files #{release_path}/public/files"
 end
 after 'deploy:update_code', :update_config
