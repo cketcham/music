ad996890b0aea72ee63c30d716549013322e1434
Cameron Ketcham <cketcham@gmail.com>
2008-07-15 02:20:07 -0700

    added play_count to songs, and changed the location of the getUpdate javascript to be in the song partial

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080611181632) do
+ActiveRecord::Schema.define(:version => 20080715083033) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20080611181632) do
     t.integer  "artist_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "play_count", :default => 0
   end
 
 end
